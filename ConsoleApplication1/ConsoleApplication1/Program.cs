2e514d56d3d2d4e1e3f6052f5d5f96a0d92f6ccc
УважановЕ <@209-STUD-11>
2017-09-10 10:41:31 +0600

    Индексатор который возводит в квадрат

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int size = 10;
+          
+            SquareArray<int> array = new SquareArray<int> (size);
+            array[0]=2;
+            Console.WriteLine("Число 2 возведенное в квадрат ={0}",array[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/SquareArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class SquareArray<T>
+    {
+        private int[] Array;
+        public int this[int i] {
+            get
+            {
+                return Array[i]; 
+            }
+            set
+            {
+                Array[i] = value*value;
+            }
+        } 
+        public SquareArray(int size){
+            Array=new int [size];
+            }
+    }
+}
